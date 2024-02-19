ebb7d5fd3c57c6be0dde8284f9742a0952fc63a5
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-02-19 18:10:29 +0530

    service card section completed

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import devImage from "./assets/devd.png";
+import frontend from "./assets/frontend.png";
+import backend from "./assets/backend.png";
 import {
   AiFillTwitterCircle,
   AiFillLinkedin,
@@ -51,6 +53,64 @@ const App = () => {
             alt="profile-image"
             className=" w-full h-full  object-cover"
           />
+        </div>
+      </section>
+      <section>
+        <div className="">
+          <h3 className="tex3xl">Services I offer</h3>
+          <p className="text-lg leading-8 text-gray-800">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+            harum consequatur distinctio molestias praesentium! Id culpa
+          </p>
+          <p className="text-lg leading-8 text-gray-800">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
+            harum consequatur distinctio molestias praesentium! Id culpa
+          </p>
+        </div>
+        <div className="">
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <img
+              src={frontend}
+              className="w-[100px] h-[100px] mx-auto"
+              alt="no-image"
+            />
+            <h3 className="text-xl font-medium pt-8 pb-2">
+              Frontend Development
+            </h3>
+            <p className="py-2">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Veritatis voluptatibus doloremque ea!
+            </p>
+            <h4 className="py-4 text-teal-600 font-semibold">
+              Designing tools I use
+            </h4>
+            <p className="text-gray-800 py-1">Reactjs</p>
+            <p className="text-gray-800 py-1">Tailwind CSS</p>
+            <p className="text-gray-800 py-1">Canva</p>
+            <p className="text-gray-800 py-1">React-Bootstrap</p>
+          </div>
+
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <img
+              src={backend}
+              className="w-[100px] h-[100px] mx-auto"
+              alt="no-image"
+            />
+            <h3 className="text-xl font-medium pt-8 pb-2">
+              Backend Development
+            </h3>
+            <p className="py-2">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Veritatis voluptatibus doloremque ea!
+            </p>
+            <h4 className="py-4 text-teal-600 font-semibold">
+              Backend tools I use
+            </h4>
+            <p className="text-gray-800 py-1">Reactjs</p>
+            <p className="text-gray-800 py-1">Tailwind CSS</p>
+            <p className="text-gray-800 py-1">Canva</p>
+            <p className="text-gray-800 py-1">React-Bootstrap</p>
+          </div>
         </div>
       </section>
     </main>
